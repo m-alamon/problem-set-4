06efab59a5223362844d703454e900a9f9288575
m-alamon <141289911+m-alamon@users.noreply.github.com>
2024-08-15 10:56:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ import src.part5_scatter as part5
 def main():
     ##  PART 1: ETL  ##
     # ETL the datasets into dataframes
+    directories = ['data/part2_plots', 'data/part3_plots', 'data/part4_plots', 'data/part5_plots']
+    part1.create_directories(directories)
+    
     pred_universe, arrest_events, charge_counts, charge_counts_by_offense = part1.extract_transform()
     
     ##  PART 2: PLOT EXAMPLES  ##
@@ -27,25 +30,27 @@ def main():
 
     ##  PART 3: BAR PLOTS AND HISTOGRAMS  ##
     # 1
-
+    part3.plot_fta_barplot(pred_universe)
     # 2
-
+    part3.plot_fta_barplot_by_sex(pred_universe)
     # 3
-
+    part3.plot_age_histogram(pred_universe)
     # 4
+    part3.plot_age_histogram_custom_bins(pred_universe)
 
     ##  PART 4: CATEGORICAL PLOTS  ##
     # 1
-    
+    part4.create_catplot_felony(pred_universe)
     # 2
-
+    part4.create_catplot_nonfelony(pred_universe)
     # 3
+    part4.create_catplot_felony_hue(pred_universe)
 
     ##  PART 5: SCATTERPLOTS  ##
     # 1
-    
+    part5.create_scatter_felony_vs_nonfelony(pred_universe)
     # 2
-
+    part5.create_scatter_felony_rearrest_vs_actual(pred_universe)
 
 if __name__ == "__main__":
     main()
